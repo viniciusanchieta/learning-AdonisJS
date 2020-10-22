4bc7a851d8bcae215d1b15664c26748fe4133f31
Vincius Cesar <contato@viniciuscesar.com>
2020-10-22 09:01:34 -0300

    Implemented function for register e consult of all in controller 'post' and added routes

--- blog/app/Controllers/Http/PostsController.ts
+++ blog/app/Controllers/Http/PostsController.ts
@@ -1,7 +1,18 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import Post from 'App/Models/Post'
 
 export default class PostsController {
-  public async index(){
-    return [];
+  public async index() { //consult all
+    const data = await Post.all();
+
+    return data;
+  }
+
+  public async store({ request }: HttpContextContract) { //register
+    const data = request.only(['title', 'content']);
+    const post = await Post.create(data);
+
+    return post;
   }
 }
