e41d217d72e66c4d4f31483f068f8c435a22b23b
Vincius Cesar <contato@viniciuscesar.com>
2020-10-21 23:36:33 -0300

    Created and implemented the migration 'posts'"

--- blog/app/Models/Post.ts
+++ blog/app/Models/Post.ts
@@ -5,6 +5,12 @@ export default class Post extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
+  @column()
+  public title: string;
+
+  @column()
+  public content: string;
+
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
